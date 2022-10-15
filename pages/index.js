10eef18e21b47d85534ae5e69a192bfe5771695a
[Kevin Jordan] <kevinjordan199449@gmail.com>
2022-10-15 11:24:06 -0400

    add fetching (need to build and approve domain)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
+import { useEffect } from "react";
 import Widget from "../components/widgets/Widget";
 
+const API_KEY = `rhoi8luXdqzFhrksISIpgW1MUB14Vmli`;
+
+async function fetchElectionData() {
+  try {
+    const result = await fetch(
+      `https://api.ap.org/v2/elections?apikey=${API_KEY}`
+    );
+
+    if (!result.ok) throw new Error("Failure to fetch data");
+
+    const data = await result.json();
+
+    console.log(data);
+  } catch (err) {
+    console.error(`Error! ${err.message}`);
+  }
+}
+
 export default function Home() {
+  useEffect(() => {
+    fetchElectionData();
+  }, []);
   return <Widget />;
 }
