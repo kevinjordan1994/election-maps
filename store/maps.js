1272904c0f2dc7842acecd9670bce6ad03afe205
[Kevin Jordan] <kevinjordan199449@gmail.com>
2022-10-28 07:07:55 -0400

    Add some maps

--- store/maps.js
+++ store/maps.js
@@ -13,9 +13,34 @@ const initialState = {
         "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4025.html",
     },
     {
+      title: "House D1",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4225.html",
+    },
+    {
+      title: "House D2",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4187.html",
+    },
+    {
       title: "House D3",
       widget:
         "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4186.html",
+    },
+    {
+      title: "House D4",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4188.html",
+    },
+    {
+      title: "House D6",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4189.html",
+    },
+    {
+      title: "House D7",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4190.html",
     },
   ],
   selectedMap: {
