da951f1173181164e3e219c76f99b4131747df86
[Kevin Jordan] <kevinjordan199449@gmail.com>
2022-10-28 08:36:42 -0400

    Finish adding maps

--- store/maps.js
+++ store/maps.js
@@ -13,6 +13,21 @@ const initialState = {
         "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4025.html",
     },
     {
+      title: "Amendment 1",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4230.html",
+    },
+    {
+      title: "Amendment 2",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4231.html",
+    },
+    {
+      title: "Amendment 3",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4233.html",
+    },
+    {
       title: "House D1",
       widget:
         "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4225.html",
@@ -41,6 +56,96 @@ const initialState = {
       title: "House D7",
       widget:
         "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4190.html",
+    },
+    {
+      title: "House D8",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4191.html",
+    },
+    {
+      title: "House D9",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4192.html",
+    },
+    {
+      title: "House D10",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4193.html",
+    },
+    {
+      title: "House D11",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4194.html",
+    },
+    {
+      title: "House D12",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4195.html",
+    },
+    {
+      title: "House D13",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4205.html",
+    },
+    {
+      title: "House D14",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4207.html",
+    },
+    {
+      title: "House D15",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4210.html",
+    },
+    {
+      title: "House D16",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4212.html",
+    },
+    {
+      title: "House D17",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4215.html",
+    },
+    {
+      title: "House D18",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4216.html",
+    },
+    {
+      title: "House D21",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4220.html",
+    },
+    {
+      title: "House D22",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4222.html",
+    },
+    {
+      title: "House D23",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4223.html",
+    },
+    {
+      title: "House D24",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4224.html",
+    },
+    {
+      title: "House D26",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4226.html",
+    },
+    {
+      title: "House D27",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4228.html",
+    },
+    {
+      title: "House D28",
+      widget:
+        "https://interactives.ap.org/election-results/customers/layouts/organization-layouts/published/77365/4229.html",
     },
   ],
   selectedMap: {
