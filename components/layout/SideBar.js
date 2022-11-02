41979f80b1f1377ba09a4b9178f7faefe6c7d7d3
FingerPrintGames <kevinjordan199449@gmail.com>
2022-11-02 10:12:42 -0400

    Change styles for sidebar

--- components/layout/SideBar.js
+++ components/layout/SideBar.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { mapsActions } from "../../store/maps";
 import styles from "./Sidebar.module.css";
@@ -5,18 +6,22 @@ import styles from "./Sidebar.module.css";
 export default function SideBar(props) {
   const maps = useSelector((state) => state.maps.maps);
   const dispatch = useDispatch();
-
-  const changeMapHandler = (e) => {
-    dispatch(mapsActions.selectMap(e.target.textContent));
-  };
+  const activeMap = useSelector((state) => state.maps.selectedMap);
 
   const mapLinks = maps.map((map) => {
     return (
-      <li key={map.title} onClick={changeMapHandler}>
-        {map.title}
-      </li>
+      <>
+        <li key={map.title} onClick={changeMapHandler}>
+          <a>{map.title}</a>
+        </li>
+        <div className={styles.map_link_container}></div>
+      </>
     );
   });
+
+  function changeMapHandler(e) {
+    dispatch(mapsActions.selectMap(e.target.textContent));
+  }
 
   return (
     <nav className={styles.sidebar}>
