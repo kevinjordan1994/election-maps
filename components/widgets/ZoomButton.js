3f68f93fcd53fb3023ded49161abb469992cd883
FingerPrintGames <kevinjordan199449@gmail.com>
2022-11-02 15:58:22 -0400

    Make zoom bar reset on new map

--- components/widgets/ZoomButton.js
+++ components/widgets/ZoomButton.js
@@ -1,6 +1,9 @@
+import { useSelector } from "react-redux";
 import styles from "./ZoomButton.module.css";
 
 export default function ZoomButton(props) {
+  const zoomLevel = useSelector((state) => state.maps.zoomLevel);
+
   const changeZoomHandler = (e) => {
     props.onZoom(e.target.value);
   };
@@ -12,6 +15,7 @@ export default function ZoomButton(props) {
       name="zoom"
       min="100"
       max="170"
+      value={zoomLevel}
       onChange={changeZoomHandler}
     />
   );
